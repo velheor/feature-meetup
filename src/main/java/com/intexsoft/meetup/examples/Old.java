a37c671ff7a216c14bb60936490f1c468c126879
Vladislav Atsman <vladislav.atsman@intexosft.by>
2024-03-10 21:47:30 +0300

    Old version text block example

---
+++ src/main/java/com/intexsoft/meetup/examples/Old.java
@@ -0,0 +1,13 @@
+package com.intexsoft.meetup.examples;
+
+public class Old {
+    public static void main(String[] args) {
+        String helloProgram = 
+                "public class Hello {\n" +
+                "   public static void main(String[] args) {\n" +
+                "    System.out.printIn(\"Hello World!\"); \n" +
+                "  }\n" +
+                "}\n";
+        System.out.println(helloProgram);
+    }
+}
